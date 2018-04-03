354be65290acd581bc5052fd21ca2ee8ac54e495
ahmad5521 <a.wv@live.com>
2018-04-03 20:29:15 +0300

    ؤخةةهف

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // JSX - javascript XML
 var template = <h1>this is JSX from app.js!</h1>;
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);                                
+ReactDOM.render(template, appRoot);                             
