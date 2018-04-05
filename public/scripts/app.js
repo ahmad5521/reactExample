df9afbc7bd3248159e9fe5fa43c304d421765b19
ahmad5521 <a.wv@live.com>
2018-04-05 17:04:18 +0300

    commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,103 +1,25 @@
-'use strict';
+"use strict";
 
-var myarrowFunction = function myarrowFunction(x) {
-    return x.split(' ')[0];
-};
-
-console.log(myarrowFunction('ahmad asiri'));
-
-var multiplier = {
-    numbers: [1, 2, 3, 4, 5, 6],
-    multplyBy: function multplyBy() {
-        return this.numbers.map(function (r) {
-            return r * r;
-        });
-    },
-    multplyByNumber: function multplyByNumber(x) {
-        return this.numbers.map(function (r) {
-            return r * x;
-        });
-    }
-};
-
-console.log(multiplier.multplyBy());
-console.log(multiplier.multplyByNumber(2));
-
-var app = {
-    title: 'React Fun',
-    subtitle: 'i like it',
-    options: ['one', 'two']
-};
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    app.options && app.options.length > 0 ? React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            app.options[0]
-        ),
-        React.createElement(
-            'li',
-            null,
-            app.options[1]
-        )
-    ) : 'no options'
-);
-
-var user = {
-    name: 'Ahmad Asiri',
-    age: 30,
-    location: ''
-};
-
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'h3',
-            null,
-            'location : ',
-            location
-        );
-    } else {
-        return React.createElement(
-            'h3',
-            null,
-            'location : Unknown'
-        );
-    }
-}
+var count = 0;
 
 var template2 = React.createElement(
-    'div',
+    "div",
     null,
     React.createElement(
-        'h1',
+        "h1",
         null,
-        'name : ',
-        user.name ? user.name : 'unknown'
+        "Count: ",
+        count
     ),
-    user.age >= 18 && React.createElement(
-        'h2',
-        null,
-        'age : ',
-        user.age
-    ),
-    getLocation(user.location)
+    React.createElement(
+        "button",
+        { className: "btn" },
+        "+1"
+    )
 );
+
+console.log(template2);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,67 +1,16 @@
 
-const myarrowFunction = (x) =>  x.split(' ')[0];
-
-console.log(myarrowFunction('ahmad asiri'));
-
-const multiplier ={
-    numbers : [1,2,3,4,5,6],
-    multplyBy() {
-        return this.numbers.map((r) => r * r)
-    },
-    multplyByNumber(x) {
-        return this.numbers.map((r) => r * x)
-    }
-};
-
-console.log(multiplier.multplyBy());
-console.log(multiplier.multplyByNumber(2));
-
-
-const app = {
-    title: 'React Fun',
-    subtitle: 'i like it',
-    options:['one', 'two']
-};
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        {(app.options && app.options.length > 0) ? 
-            (<ol>
-                <li>{app.options[0]}</li>
-                <li>{app.options[1]}</li>
-            </ol>)
-            : 'no options'}
-    </div>
-    
-    );
- 
-const user = {
-    name: 'Ahmad Asiri',
-    age: 30,
-    location: ''
-};
-
-function getLocation(location){
-    if(location){
-        return <h3>location : {location}</h3>;
-    }else{
-        return <h3>location : Unknown</h3>;
-    }    
-}
+let count = 0;
 
 const template2 = (
     <div>
-        <h1>name : {user.name ? user.name : 'unknown'}</h1>
-        {(user.age >= 18) && <h2>age : {user.age}</h2>}
-        {getLocation(user.location)}
-    </div>
+        <h1>Count: {count}</h1>
+        <button className="btn">+1</button>
+    </div>    
+);
     
-    );
-    
+console.log(template2);
 
+const appRoot = document.getElementById('app');
 
-    const appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);                             
+ReactDOM.render(template2, appRoot);                             
 
